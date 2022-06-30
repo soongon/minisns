4c36444985ec45c14110a45c6b714da45373ee92
soongonkim <soongon@hucloud.co.kr>
2022-06-30 15:18:20 +0900

    mybatis 실행

--- src/main/java/kr/re/kitri/minisns/repository/PostRepository.java
+++ src/main/java/kr/re/kitri/minisns/repository/PostRepository.java
@@ -2,6 +2,7 @@ package kr.re.kitri.minisns.repository;
 
 import kr.re.kitri.minisns.model.Article;
 import kr.re.kitri.minisns.model.Post;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Connection;
@@ -10,22 +11,11 @@ import java.util.Arrays;
 import java.util.List;
 
 @Repository
-public class PostRepository {
-    public List<Post> selectAllPosts() {
-        List<Post> posts = Arrays.asList(
-                new Post(1, "첫번째 글", "kim", "처음 쓰는 글이에요", 0),
-                new Post(2, "두번째 글", "lee", "두번째 쓰는 글이에요", 15),
-                new Post(3, "세번째 글", "park", "세번째 쓰는 글이에요", 20)
-        );
-        return posts;
+@Mapper
+public interface PostRepository {
+    List<Post> selectAllPosts();
 
-    }
+    Post selectPostById(long postId);
 
-    public Post selectPostById(long postId) {
-        return new Post(postId, "제목입니다.", "lee", "조회하신 내용입니다.", 0);
-    }
-
-    public Post insertPost(Post post) {
-        return post;
-    }
+    Post insertPost(Post post);
 }
